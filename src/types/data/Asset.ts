ccb7a2162b3a8a3025c64bf2aab833bdfc4c5785
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-03-05 17:07:07 +0100

    Revert changes to Asset types

--- src/types/data/Asset.ts
+++ src/types/data/Asset.ts
@@ -9,4 +9,16 @@ export declare type Asset = {
   size?: number;
   mimeType?: string;
   url?: string; // Field added at runtime by GraphCMS asset's provider - See https://www.filestack.com/
+
+  // XXX Additional fields that do not exist on the native GraphCMS Asset model, but you can add them and they'll be handled when using GraphCMSAsset, for instance
+  alt?: string;
+  classes?: string;
+  defaultTransformations?: object;
+  importUrl?: string;
+  key?: string;
+  linkTarget?: string;
+  linkUrl?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  style?: string | object | any;
+  title?: string;
 } & GraphCMSSystemFields;
---
+++ src/types/data/AssetTransformations.ts
@@ -0,0 +1,7 @@
+import { GraphCMSSystemFields } from './GraphCMSSystemFields';
+
+export declare type AssetTransformations = {
+  id?: string;
+  height?: number;
+  width?: number;
+} & GraphCMSSystemFields;
