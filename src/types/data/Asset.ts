725aa2759c93d1b158290ef673d97f83060dba7e
Vadorequest <ambroise.dhenain@gmail.com>
2020-03-05 17:53:31 +0100

    Cleanup unused types (#6)
    * Cleanup unused types
    
    * Misc doc
    
    * Revert changes to Asset types

--- src/types/data/Asset.ts
+++ src/types/data/Asset.ts
@@ -2,21 +2,23 @@ import { GraphCMSSystemFields } from './GraphCMSSystemFields';
 
 export declare type Asset = {
   id?: string;
+  handle?: string;
+  fileName?: string;
+  height?: number | string;
+  width?: number | string;
+  size?: number;
+  mimeType?: string;
+  url?: string; // Field added at runtime by GraphCMS asset's provider - See https://www.filestack.com/
+
+  // XXX Additional fields that do not exist on the native GraphCMS Asset model, but you can add them and they'll be handled when using GraphCMSAsset, for instance
   alt?: string;
   classes?: string;
   defaultTransformations?: object;
-  fileName?: string;
-  handle?: string;
-  height?: number | string;
   importUrl?: string;
   key?: string;
   linkTarget?: string;
   linkUrl?: string;
-  mimeType?: string;
-  size?: number;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   style?: string | object | any;
   title?: string;
-  url?: string;
-  width?: number | string;
 } & GraphCMSSystemFields;
