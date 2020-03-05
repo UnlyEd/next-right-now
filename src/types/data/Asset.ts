1d615e097c16a587b38dfe35c6810694f8de413e
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-03-05 07:24:21 +0100

    Cleanup unused types

--- src/types/data/Asset.ts
+++ src/types/data/Asset.ts
@@ -2,21 +2,11 @@ import { GraphCMSSystemFields } from './GraphCMSSystemFields';
 
 export declare type Asset = {
   id?: string;
-  alt?: string;
-  classes?: string;
-  defaultTransformations?: object;
-  fileName?: string;
   handle?: string;
+  fileName?: string;
   height?: number | string;
-  importUrl?: string;
-  key?: string;
-  linkTarget?: string;
-  linkUrl?: string;
-  mimeType?: string;
-  size?: number;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  style?: string | object | any;
-  title?: string;
-  url?: string;
   width?: number | string;
+  size?: number;
+  mimeType?: string;
+  url?: string; // Field added at runtime by GraphCMS asset's provider - See https://www.filestack.com/
 } & GraphCMSSystemFields;
--- src/types/data/AssetTransformations.ts
+++
@@ -1,7 +0,0 @@
-import { GraphCMSSystemFields } from './GraphCMSSystemFields';
-
-export declare type AssetTransformations = {
-  id?: string;
-  height?: number;
-  width?: number;
-} & GraphCMSSystemFields;
