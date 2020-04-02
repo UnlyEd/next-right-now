994d8845f19968c7ce62287b733db83e7fbe898d
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-04-02 21:41:57 +0200

    Improve examples

--- src/pages/examples.tsx
+++ src/pages/examples.tsx
@@ -100,6 +100,44 @@ const Examples: NextPage<PageProps> = (props: PageProps): JSX.Element => {
 
             <hr />
 
+            <div>
+              <h2 className={'pcolor'}>Analytics front-end examples</h2>
+
+              Log event on&nbsp;
+              <a
+                href={'https://github.com/amplitude/react-amplitude#children'}
+                target={'_blank'}
+                rel={'noopener'}
+              >
+                link click
+              </a>
+              <br />
+              <code>
+                {`
+                  <a href="{'/examples'}" onClick={() => { logEvent('open-examples'); }}>Open</a>
+                `}
+              </code>
+              <br />
+              <br />
+
+              Log event on&nbsp;
+              <a
+                href={'https://github.com/amplitude/react-amplitude#logonmount-props'}
+                target={'_blank'}
+                rel={'noopener'}
+              >
+                component mount
+              </a> (once only)
+              <br />
+              <code>
+                {`
+                  <LogOnMount eventType="page-displayed" />
+                `}
+              </code>
+            </div>
+
+            <hr />
+
             <div
               css={css`
                 .product-container {
@@ -114,7 +152,7 @@ const Examples: NextPage<PageProps> = (props: PageProps): JSX.Element => {
                 }
               `}
             >
-              <h2 className={'pcolor'}>GraphQL & GraphCMS examples</h2>
+              <h2 className={'pcolor'}>GraphQL & GraphCMS universal examples</h2>
               <blockquote>Fetching products from GraphCMS API</blockquote>
               <div>
                 The below products are fetched from GraphCMS API, using GraphQL and Apollo.<br />
@@ -163,88 +201,132 @@ const Examples: NextPage<PageProps> = (props: PageProps): JSX.Element => {
 
             <hr />
 
-            <h2 className={'pcolor'}>i18n examples</h2>
-            <Alert color={'info'}>
-              <div>
-                Each example shows the rendered version and its code snippet.<br />
-                The goal is to showcase real-world examples to help you get started faster and give a wider overview of what's possible.<br />
-                <a href={'https://react.i18next.com/'} target="blank" rel={'nofollow noreferrer'}>
-                  Check the official documentation
-                </a>
-              </div>
-            </Alert>
+            <div>
+              <h2 className={'pcolor'}>Monitoring universal examples</h2>
 
-            <Container>
-              <div>
-                {t('examples.i18n.simpleTranslation', 'Traduction simple')}<br />
-                <code>{'{t(\'examples.i18n.simpleTranslation\', \'Traduction simple\')}'}</code>
-              </div>
-              <hr />
+              Log runtime exception<br />
+              <code>
+                {`try{throw new Error('test')}catch(e){Sentry.captureException(e)}`}
+              </code>
+              <br />
+              <br />
 
-              <div>
-                {t('examples.i18n.pluralTranslation', 'Traduction avec gestion du pluriel', { count: 1 })}<br />
-                <code>{'{t(\'examples.i18n.pluralTranslation\', \'Traduction avec gestion du pluriel\', { count: 1 })}'}</code>
-              </div>
-              <div>
-                {t('examples.i18n.pluralTranslation', 'Traduction avec gestion du pluriel', { count: 2 })}<br />
-                <code>{'{t(\'examples.i18n.pluralTranslation\', \'Traduction avec gestion du pluriel\', { count: 2 })}'}</code>
-              </div>
-              <hr />
+              Log message<br />
+              <code>
+                {`Sentry.captureMessage(warning, Sentry.Severity.Warning);`}
+              </code>
+              <br />
+              <br />
 
-              <div>
-                <Trans
-                  i18nKey={'examples.i18n.dynamicTranslation'}
-                >
-                  Contenu dynamique : <b>{{ uuid: uuid() }}</b>
-                </Trans>
-                <br />
-                <code>
-                  {'<Trans\n' +
-                  '  i18nKey="{\'examples.i18n.dynamicTranslation\'}"\n' +
-                  '>\n' +
-                  '  Contenu dynamique : <b>{{ uuid: uuid() }}</b>\n' +
-                  '</Trans>'}
-                </code>
-              </div>
-              <hr />
+              Severity examples<br />
+              <code>Severity.Fatal</code>
+              <code>Severity.Error</code>
+              <code>Severity.Warning</code>
+              <code>Severity.Log</code>
+              <code>Severity.Info</code>
+              <code>Severity.Debug</code>
+              <code>Severity.Critical</code>
+              <br />
+              <br />
 
-              <div>
-                <Trans
-                  i18nKey={'examples.i18n.dynamicPluralTranslation'}
-                  count={1}
-                >
-                  Nous avons trouvé {{ count: 1 }} solution pour vous.
-                </Trans>
-                <br />
-                <code>
-                  {'<Trans\n' +
-                  '  i18nKey="{\'examples.i18n.dynamicPluralTranslation\'}"\n' +
-                  '  count="{1}"\n' +
-                  '>\n' +
-                  '  Nous avons trouvé {{ count: 1 }} solution pour vous.\n' +
-                  '</Trans>'}
-                </code>
-              </div>
-              <div>
-                <Trans
-                  i18nKey={'examples.i18n.dynamicPluralTranslation'}
-                  count={2}
-                >
-                  Nous avons trouvé {{ count: 2 }} solution pour vous.
-                </Trans>
-                <br />
-                <code>
-                  {'<Trans\n' +
-                  '  i18nKey="{\'examples.i18n.dynamicPluralTranslation\'}"\n' +
-                  '  count="{2}"\n' +
-                  '>\n' +
-                  '  Nous avons trouvé {{ count: 2 }} solution pour vous.\n' +
-                  '</Trans>'}
-                </code>
-              </div>
-              <hr />
+              <a
+                href={'https://docs.sentry.io/enriching-error-data/breadcrumbs'}
+                target={'_blank'}
+                rel={'noopener'}
+              >
+                Breadcrumbs documentation
+              </a><br />
+              <code>
+                {`Sentry.addBreadcrumb({category: fileLabel, message: 'Rendering'})`}
+              </code>
+            </div>
 
-            </Container>
+            <hr />
+
+            <div>
+              <h2 className={'pcolor'}>I18n universal examples <small>(using Locize 3rd party vendor)</small></h2>
+              <Alert color={'info'}>
+                <div>
+                  Each example shows the rendered version and its code snippet.<br />
+                  The goal is to showcase real-world examples to help you get started faster and give a wider overview of what's possible.<br />
+                  <a href={'https://react.i18next.com/'} target="blank" rel={'nofollow noreferrer'}>
+                    Check the official documentation
+                  </a>
+                </div>
+              </Alert>
+
+              <Container>
+                <div>
+                  {t('examples.i18n.simpleTranslation', 'Traduction simple')}<br />
+                  <code>{'{t(\'examples.i18n.simpleTranslation\', \'Traduction simple\')}'}</code>
+                </div>
+                <hr />
+
+                <div>
+                  {t('examples.i18n.pluralTranslation', 'Traduction avec gestion du pluriel', { count: 1 })}<br />
+                  <code>{'{t(\'examples.i18n.pluralTranslation\', \'Traduction avec gestion du pluriel\', { count: 1 })}'}</code>
+                </div>
+                <div>
+                  {t('examples.i18n.pluralTranslation', 'Traduction avec gestion du pluriel', { count: 2 })}<br />
+                  <code>{'{t(\'examples.i18n.pluralTranslation\', \'Traduction avec gestion du pluriel\', { count: 2 })}'}</code>
+                </div>
+                <hr />
+
+                <div>
+                  <Trans
+                    i18nKey={'examples.i18n.dynamicTranslation'}
+                  >
+                    Contenu dynamique : <b>{{ uuid: uuid() }}</b>
+                  </Trans>
+                  <br />
+                  <code>
+                    {'<Trans\n' +
+                    '  i18nKey="{\'examples.i18n.dynamicTranslation\'}"\n' +
+                    '>\n' +
+                    '  Contenu dynamique : <b>{{ uuid: uuid() }}</b>\n' +
+                    '</Trans>'}
+                  </code>
+                </div>
+                <hr />
+
+                <div>
+                  <Trans
+                    i18nKey={'examples.i18n.dynamicPluralTranslation'}
+                    count={1}
+                  >
+                    Nous avons trouvé {{ count: 1 }} solution pour vous.
+                  </Trans>
+                  <br />
+                  <code>
+                    {'<Trans\n' +
+                    '  i18nKey="{\'examples.i18n.dynamicPluralTranslation\'}"\n' +
+                    '  count="{1}"\n' +
+                    '>\n' +
+                    '  Nous avons trouvé {{ count: 1 }} solution pour vous.\n' +
+                    '</Trans>'}
+                  </code>
+                </div>
+                <div>
+                  <Trans
+                    i18nKey={'examples.i18n.dynamicPluralTranslation'}
+                    count={2}
+                  >
+                    Nous avons trouvé {{ count: 2 }} solution pour vous.
+                  </Trans>
+                  <br />
+                  <code>
+                    {'<Trans\n' +
+                    '  i18nKey="{\'examples.i18n.dynamicPluralTranslation\'}"\n' +
+                    '  count="{2}"\n' +
+                    '>\n' +
+                    '  Nous avons trouvé {{ count: 2 }} solution pour vous.\n' +
+                    '</Trans>'}
+                  </code>
+                </div>
+                <hr />
+
+              </Container>
+            </div>
           </Container>
         </>
       )}
