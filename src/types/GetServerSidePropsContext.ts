84569a517348844e023aada20933398cdbadc161
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-04-26 18:16:56 +0200

    Add missing Next.js TS types

---
+++ src/types/GetServerSidePropsContext.ts
@@ -0,0 +1,17 @@
+import { IncomingMessage, ServerResponse } from 'http';
+import { ParsedUrlQuery } from 'querystring';
+
+/**
+ * Context type used by "getServerSideProps"
+ *
+ * @see https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
+ * @see node_modules/next/types/index.d.ts
+ */
+export declare type GetServerSidePropsContext<Q extends ParsedUrlQuery = ParsedUrlQuery> = {
+  req: IncomingMessage;
+  res: ServerResponse;
+  params?: Q;
+  query: ParsedUrlQuery;
+  preview?: boolean;
+  previewData?: any;
+};
---
+++ src/types/GetStaticPropsContext.ts
@@ -0,0 +1,13 @@
+import { ParsedUrlQuery } from 'querystring';
+
+/**
+ * Context type used by "getStaticProps"
+ *
+ * @see https://nextjs.org/docs/basic-features/data-fetching#getstaticprops-static-generation
+ * @see node_modules/next/types/index.d.ts
+ */
+export declare type GetStaticPropsContext<Q extends ParsedUrlQuery = ParsedUrlQuery> = {
+  params?: Q
+  preview?: boolean
+  previewData?: any
+};
