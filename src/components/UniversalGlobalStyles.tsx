427f93616db8ad8b52fca41c0d58c51f7986e417
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-05-05 09:41:15 +0200

    Refactor CSS global styles into UniversalGlobalStyles (code quality)

---
+++ src/components/UniversalGlobalStyles.tsx
@@ -0,0 +1,247 @@
+/** @jsx jsx */
+import { css, Global, jsx } from '@emotion/core';
+
+import { NRN_DEFAULT_FONT, NRN_DEFAULT_SECONDARY_COLOR } from '../constants';
+import { Theme } from '../types/data/Theme';
+
+type Props = {
+  theme: Theme;
+}
+
+/**
+ * Those styles are applied
+ *  - universally (browser + server)
+ *  - globally (applied to all pages), through Layouts
+ *
+ * XXX Note that primaryColor, primaryAltColor and secondaryColor don't necessarily follow best practices regarding colors management.
+ *  I personally recommend to take a look at https://material.io/design/color/the-color-system.html#color-theme-creation, those guidelines may fit your use-case
+ *  Don't hesitate to share best practices around those, this does the job for simple use-cases
+ *
+ * @param props
+ */
+const UniversalGlobalStyles: React.FunctionComponent<Props> = (props): JSX.Element => {
+  const { theme } = props;
+  const { primaryColor } = theme;
+  const primaryAltColor = primaryColor; // Helper for "primary alternative color", for customers with 2 primary colors (currently unused)
+  const secondaryColor = NRN_DEFAULT_SECONDARY_COLOR;
+  const primaryFont = NRN_DEFAULT_FONT; // You could allow custom font per customer from Theme.font (that's what we do in our proprietary app)
+
+  return (
+    <Global
+      styles={css`
+        // Only applied to the main application
+        body.nrn {
+          background-color: #f5f5f5;
+
+          #__next{
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+          }
+        }
+
+        // Applied to all containers marked with ".nrn" - XXX could be grouped with the other one above?
+        .nrn {
+          * {
+            font-family: "${primaryFont}", sans-serif !important;
+          }
+
+          .container {
+            justify-content: center;
+            text-align: center;
+            margin-left: auto;
+            margin-right: auto;
+          }
+
+          .container-white {
+            background-color: white;
+            border-radius: 10px;
+            padding: 30px;
+            margin-top: 30px;
+            margin-bottom: 30px;
+          }
+
+          // ----------- Utilities -----------
+
+          b, .b, strong {
+            color: ${primaryColor};
+            font-weight: bold;
+          }
+
+          a {
+           color: ${primaryColor} !important;
+          }
+
+          .page-container {
+            background-color: #f5f5f5;
+
+            @media (max-width: 991.98px) {
+              min-height: 300px;
+            }
+          }
+
+          .center {
+            text-align: center;
+          }
+
+          .center-block {
+            text-align: center;
+            margin: auto;
+          }
+
+          .pcolor, [class*="primary_color"] {
+            color: ${primaryColor};
+            fill: ${primaryColor}; // For SVG
+          }
+
+          .pacolor {
+            color: ${primaryAltColor};
+          }
+
+          .scolor {
+            color: ${secondaryColor};
+          }
+
+          .pbgcolor {
+            background-color: ${primaryColor};
+          }
+
+          .pabgcolor {
+            background-color: ${primaryAltColor};
+          }
+
+          .sbgcolor {
+            background-color: ${secondaryColor};
+          }
+
+          .btn-border{
+             background-color: transparent;
+             color: ${theme.primaryColor};
+             border: 1.5px solid ${theme.primaryColor};
+             border-radius: 30px;
+             margin: 5px;
+             padding: 5px 12px 5px 12px;
+             white-space: nowrap;
+             display: inline-block; // Necessary so that <a> and <button> behave identically
+          }
+
+          label {
+            cursor: pointer;
+          }
+
+          button {
+            cursor: pointer;
+            outline: none !important; // Overrides bootstrap color around the button
+
+            &.btn-primary {
+              color: ${secondaryColor};
+              background-color: ${primaryAltColor};
+              border-color: ${primaryAltColor};
+
+              &:active, &:focus {
+                box-shadow: 0 0 0 0.2rem ${primaryAltColor};
+              }
+            }
+
+            &.btn-outline-secondary {
+              color: ${primaryAltColor};
+              background-color: ${secondaryColor};
+
+              &:active, &:focus {
+                box-shadow: 0 0 0 0.2rem ${secondaryColor};
+              }
+            }
+
+            &.btn-primary, &.btn-outline-secondary {
+              &:hover,
+              &:active,
+              &:focus {
+                opacity: 0.8;
+              }
+            }
+
+            &.disabled {
+              cursor: not-allowed;
+            }
+          }
+
+          .info-label {
+            display: inline-block;
+            border-radius: 60px;
+            border: none;
+            background-color: #C9D0F6;
+            color: #0028FF;
+            padding: 10px 15px 7px 14px;
+            margin: 1px;
+          }
+
+          .select {
+            // Overrides react select styles everywhere
+            *  {
+              color: ${primaryColor} !important;
+            }
+          }
+
+          [class*="fa-"], [class*="fal-"], [class*="fas-"], [class*="far-"] {
+            margin-right: 5px;
+          }
+
+          .animated {
+            // Delay control (latency)
+            &.delay-100ms {
+              animation-delay: 0.1s;
+            }
+
+            &.delay-200ms {
+              animation-delay: 0.2s;
+            }
+
+            &.delay-400ms {
+              animation-delay: 0.4s;
+            }
+
+            &.delay-600ms {
+              animation-delay: 0.6s;
+            }
+
+            // Duration control (speed)
+            &.duration-100ms {
+              animation-duration: 0.1s;
+            }
+
+            &.duration-200ms {
+              animation-duration: 0.2s;
+            }
+
+            &.duration-300ms {
+              animation-duration: 0.3s;
+            }
+
+            &.duration-400ms {
+              animation-duration: 0.4s;
+            }
+
+            &.duration-600ms {
+              animation-duration: 0.6s;
+            }
+
+            &.duration-3000ms {
+              animation-duration: 3s;
+            }
+
+            &.duration-6000ms {
+              animation-duration: 6s;
+            }
+          }
+
+          .fade {
+            opacity: 1 !important; // Overrides default bootstrap behaviour to avoid make-believe SSR doesn't work on the demo, when JS is disabled - See https://github.com/UnlyEd/next-right-now/issues/9
+          }
+        }
+      `}
+    />
+  );
+};
+
+export default UniversalGlobalStyles;
--- src/components/UniversalPageLayout.tsx
+++ src/components/UniversalPageLayout.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { LogOnMount } from '@amplitude/react-amplitude';
-import { css, Global, jsx } from '@emotion/core';
+import { css, jsx } from '@emotion/core';
 import * as Sentry from '@sentry/node';
 import { isBrowser } from '@unly/utils';
 import { createLogger } from '@unly/utils-simple-logger';
@@ -8,14 +8,12 @@ import classnames from 'classnames';
 import { ThemeProvider } from 'emotion-theming';
 import React from 'react';
 import { Button } from 'reactstrap';
-
-import { NRN_DEFAULT_FONT, NRN_DEFAULT_SECONDARY_COLOR } from '../constants';
 import ErrorPage from '../pages/_error';
 import { PageLayoutProps } from '../types/PageLayoutProps';
-import { getValue, STRATEGY_DO_NOTHING } from '../utils/record';
 import Footer from './Footer';
 import Head from './Head';
 import Nav from './Nav';
+import UniversalGlobalStyles from './UniversalGlobalStyles';
 
 const fileLabel = 'components/UniversalPageLayout';
 const logger = createLogger({ // eslint-disable-line no-unused-vars,@typescript-eslint/no-unused-vars
@@ -67,221 +65,7 @@ const UniversalPageLayout = (props: Props): JSX.Element => {
       <LogOnMount eventType="page-displayed" />
 
       {/* XXX Global styles that applies to all pages within this layout go there */}
-      <Global
-        styles={css`
-          // Only applied to the main application
-          body.nrn {
-            background-color: #f5f5f5;
-
-            #__next{
-              min-height: 100vh;
-              display: flex;
-              flex-direction: column;
-              justify-content: space-between;
-            }
-          }
-
-          // Applied to all containers marked with ".nrn" - XXX could be grouped with the other one above?
-          .nrn {
-            * {
-              // XXX We try to load the font specified by the organisation or the school, then we fallback to one of the "less ugly" native font
-              font-family: "${getValue(theme, `font`, NRN_DEFAULT_FONT, STRATEGY_DO_NOTHING)}", sans-serif !important;
-            }
-
-            .container {
-              justify-content: center;
-              text-align: center;
-              margin-left: auto;
-              margin-right: auto;
-            }
-
-            .container-white {
-              background-color: white;
-              border-radius: 10px;
-              padding: 30px;
-              margin-top: 30px;
-              margin-bottom: 30px;
-            }
-
-            // ----------- Utilities -----------
-
-            b, .b, strong {
-              color: ${getValue(theme, `primaryColor`)};
-              font-weight: bold;
-            }
-
-            a {
-             color: ${getValue(theme, `primaryColor`)} !important;
-            }
-
-            .page-container {
-              background-color: #f5f5f5;
-
-              @media (max-width: 991.98px) {
-                min-height: 300px;
-              }
-            }
-
-            .center {
-              text-align: center;
-            }
-
-            .center-block {
-              text-align: center;
-              margin: auto;
-            }
-
-            .pcolor, [class*="primary_color"] {
-              color: ${getValue(theme, `primaryColor`)};
-              fill: ${getValue(theme, `primaryColor`)}; // For SVG
-            }
-
-            .pacolor {
-              color: ${getValue(theme, `primaryAltColor`, getValue(theme, `primaryColor`), STRATEGY_DO_NOTHING)};
-            }
-
-            .scolor {
-              color: ${getValue(theme, `secondaryColor`, NRN_DEFAULT_SECONDARY_COLOR, STRATEGY_DO_NOTHING)};
-            }
-
-            .pbgcolor {
-              background-color: ${getValue(theme, `primaryColor`)};
-            }
-
-            .pabgcolor {
-              background-color: ${getValue(theme, `primaryAltColor`, getValue(theme, `primaryColor`), STRATEGY_DO_NOTHING)};
-            }
-
-            .sbgcolor {
-              background-color: ${getValue(theme, `secondaryColor`, NRN_DEFAULT_SECONDARY_COLOR, STRATEGY_DO_NOTHING)};
-            }
-
-            .btn-border{
-               background-color: transparent;
-               color: ${theme.primaryColor};
-               border: 1.5px solid ${theme.primaryColor};
-               border-radius: 30px;
-               margin: 5px;
-               padding: 5px 12px 5px 12px;
-               white-space: nowrap;
-               display: inline-block; // Necessary so that <a> and <button> behave identically
-            }
-
-            label {
-              cursor: pointer;
-            }
-
-            button {
-              cursor: pointer;
-              outline: none !important; // Overrides bootstrap color around the button
-
-              &.btn-primary {
-                color: ${getValue(theme, `secondaryColor`, NRN_DEFAULT_SECONDARY_COLOR, STRATEGY_DO_NOTHING)};
-                background-color: ${getValue(theme, `primaryAltColor`, getValue(theme, `primaryColor`), STRATEGY_DO_NOTHING)};
-                border-color: ${getValue(theme, `primaryAltColor`, getValue(theme, `primaryColor`), STRATEGY_DO_NOTHING)};
-
-                &:active, &:focus {
-                  box-shadow: 0 0 0 0.2rem ${getValue(theme, `primaryAltColor`, getValue(theme, `primaryColor`), STRATEGY_DO_NOTHING)};
-                }
-              }
-
-              &.btn-outline-secondary {
-                color: ${getValue(theme, `primaryAltColor`, getValue(theme, `primaryColor`), STRATEGY_DO_NOTHING)};
-                background-color: ${getValue(theme, `secondaryColor`, NRN_DEFAULT_SECONDARY_COLOR, STRATEGY_DO_NOTHING)};
-
-                &:active, &:focus {
-                  box-shadow: 0 0 0 0.2rem ${getValue(theme, `secondaryColor`, NRN_DEFAULT_SECONDARY_COLOR, STRATEGY_DO_NOTHING)};
-                }
-              }
-
-              &.btn-primary, &.btn-outline-secondary {
-                &:hover,
-                &:active,
-                &:focus {
-                  opacity: 0.8;
-                }
-              }
-
-              &.disabled {
-                cursor: not-allowed;
-              }
-            }
-
-            .info-label {
-              display: inline-block;
-              border-radius: 60px;
-              border: none;
-              background-color: #C9D0F6;
-              color: #0028FF;
-              padding: 10px 15px 7px 14px;
-              margin: 1px;
-            }
-
-            .select {
-              // Overrides react select styles everywhere
-              *  {
-                color: ${getValue(theme, `primaryColor`, NRN_DEFAULT_SECONDARY_COLOR, STRATEGY_DO_NOTHING)} !important;
-              }
-            }
-
-            [class*="fa-"], [class*="fal-"], [class*="fas-"], [class*="far-"] {
-              margin-right: 5px;
-            }
-
-            .animated {
-              // Delay control (latency)
-              &.delay-100ms {
-                animation-delay: 0.1s;
-              }
-
-              &.delay-200ms {
-                animation-delay: 0.2s;
-              }
-
-              &.delay-400ms {
-                animation-delay: 0.4s;
-              }
-
-              &.delay-600ms {
-                animation-delay: 0.6s;
-              }
-
-              // Duration control (speed)
-              &.duration-100ms {
-                animation-duration: 0.1s;
-              }
-
-              &.duration-200ms {
-                animation-duration: 0.2s;
-              }
-
-              &.duration-300ms {
-                animation-duration: 0.3s;
-              }
-
-              &.duration-400ms {
-                animation-duration: 0.4s;
-              }
-
-              &.duration-600ms {
-                animation-duration: 0.6s;
-              }
-
-              &.duration-3000ms {
-                animation-duration: 3s;
-              }
-
-              &.duration-6000ms {
-                animation-duration: 6s;
-              }
-            }
-
-            .fade {
-              opacity: 1 !important; // Overrides default bootstrap behaviour to avoid make-believe SSR doesn't work on the demo, when JS is disabled - See https://github.com/UnlyEd/next-right-now/issues/9
-            }
-          }
-        `}
-      />
+      <UniversalGlobalStyles theme={theme} />
 
       <ThemeProvider theme={theme}>
         {/*
