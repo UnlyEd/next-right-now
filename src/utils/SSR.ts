b7055a0f4235b97a1667743e440e8160b529bcf4
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-05-07 02:09:43 +0200

    Doc

--- src/utils/SSR.ts
+++ src/utils/SSR.ts
@@ -30,7 +30,7 @@ export type GetCommonServerSidePropsResults = Omit<UniversalSSRPageProps, 'apoll
  * Only executed on the server side, for every request.
  * Computes some dynamic props that should be available for all SSR pages that use getServerSideProps
  *
- * Because the exact GQL query will depend on the consumer, this helper doesn't run any query by itself, but rather return all necessary props to allow the consumer to perform its own queries
+ * Because the exact GQL query will depend on the consumer (AKA "caller"), this helper doesn't run any query by itself, but rather return all necessary props to allow the consumer to perform its own queries
  * This improves performances, by only running one GQL query instead of many (consumer's choice)
  *
  * Meant to avoid code duplication
@@ -77,6 +77,8 @@ export const getCommonServerSideProps = async (context: GetServerSidePropsContex
     },
   };
 
+  // Most props returned here will be necessary for the app to work properly (see "UniversalSSRPageProps")
+  // Some props are meant to be helpful to the consumer and won't be passed down to the _app.render (e.g: apolloClient, layoutQueryOptions)
   return {
     apolloClient,
     bestCountryCodes,
