5f66aafe36e37de441b2c3e5a6abeeb54099e55c
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-05-09 01:45:45 +0200

    Ignore noisy warning

--- src/utils/app/ignoreNoisyWarningsHacks.ts
+++ src/utils/app/ignoreNoisyWarningsHacks.ts
@@ -7,6 +7,9 @@ console.error = (...args): void => {
   if (/Warning.*Function components cannot be given refs/.test(args[0])) {
     // HACK: Muting error, fix as soon as https://github.com/zeit/next.js/issues/7915 gets resolved
     return;
+  }else if (/Warning.*Expected server HTML to contain a matching/.test(args[0]) && /Footer/.test(args[3])) {
+    // XXX Silent false-positive warning (server doesn't render that part because it can't on server when using SSG, perfectly normal and not an actual issue)
+    return;
   }
   originalError.call(console, ...args);
 };
