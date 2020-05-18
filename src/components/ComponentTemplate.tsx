df4d6f5b430e9e0509abcced5e5decdb4d7d3260
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-05-18 22:33:22 +0200

    Add component template to get started quickly

---
+++ src/components/ComponentTemplate.tsx
@@ -0,0 +1,29 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import React from 'react';
+import Text from './utils/Text';
+
+type Props = {}
+
+/**
+ * This component is a template meant to be duplicated to quickly get started with new React components.
+ *
+ * @param props
+ */
+const ComponentTemplate: React.FunctionComponent<Props> = (props): JSX.Element => {
+  return (
+    <Text
+      css={css`
+        margin: 30px;
+    `}
+    >
+      {`
+        This component is a template meant to be duplicated to quickly get started with new React components.
+
+        Feel free to adapt it at your convenience
+      `}
+    </Text>
+  );
+};
+
+export default ComponentTemplate;
