b54f416c6626a8de7bba0087e6203d55286be9d8
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-05-26 18:32:37 +0200

    WDYR (why-did-you-render) helps locate unnecessary re-renders and fix them

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,6 +27,22 @@ library.add(
 );
 
 /**
+ * WDYR (why-did-you-render) helps locate unnecessary re-renders and fix them
+ * Applied in development environment, on the frontend only
+ *
+ * @see https://github.com/welldone-software/why-did-you-render
+ */
+if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  // eslint-disable-next-line no-console
+  console.debug('Applying whyDidYouRender, to help you locate unnecessary re-renders during development. See https://github.com/welldone-software/why-did-you-render');
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+  });
+}
+
+/**
  * "props.pageProps" will depend on whether the page is served by server or client, SSG or SSR
  * (MultiversalAppBootstrapProps<SSGPageProps> | MultiversalAppBootstrapProps<SSRPageProps>) is basically a superset of AppProps (from 'next/app')
  */
