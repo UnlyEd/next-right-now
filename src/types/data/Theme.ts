7e8243e7046c149b34e22c8952c682810cadd8c1
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-06-04 00:59:58 +0200

    Fix ts type

--- src/types/data/Theme.ts
+++ src/types/data/Theme.ts
@@ -5,6 +5,6 @@ import { Customer } from './Customer';
 export declare type Theme = {
   customer: AirtableRecord<Customer> | string[]; // Stored as an array of strings on AT API, converted to a Customer object once sanitised
   id?: number;
-  logo?: AirtableRecord<Asset> | [AirtableRecord<Asset>]; // Stored as an array on AT API, converted to an Asset object once sanitised
+  logo?: AirtableRecord<Asset> | AirtableRecord<Asset>[]; // Stored as an array on AT API, converted to an Asset object once sanitised
   primaryColor?: string;
 };
