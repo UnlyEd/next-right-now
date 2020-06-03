fc765c0fe01ecb0e25e67232db0d6275f3ad9277
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-06-04 00:32:12 +0200

    Refactor (buildDataset util)

--- src/utils/api/fetchCustomer.ts
+++ src/utils/api/fetchCustomer.ts
@@ -1,10 +1,10 @@
 import find from 'lodash.find';
-import map from 'lodash.map';
 import { AirtableDataset } from '../../types/data/AirtableDataset';
 import { AirtableRecord } from '../../types/data/AirtableRecord';
 import { Customer } from '../../types/data/Customer';
 import { Product } from '../../types/data/Product';
 import { Theme } from '../../types/data/Theme';
+import { buildDataset } from '../data/airtableDataset';
 import { sanitizeRecord } from '../data/airtableRecord';
 import fetchAirtableTable, { GenericListApiResponse } from './fetchAirtableTable';
 
@@ -18,19 +18,11 @@ const fetchCustomer = async (preferredLocales: string[]): Promise<AirtableRecord
   const { records: airtableCustomers } = await fetchAirtableTable<GenericListApiResponse<AirtableRecord<Customer>>>('Customer');
   const { records: airtableThemes } = await fetchAirtableTable<GenericListApiResponse<AirtableRecord<Theme>>>('Theme');
   const { records: airtableProducts } = await fetchAirtableTable<GenericListApiResponse<AirtableRecord<Product>>>('Product');
-  const dataset: AirtableDataset = {};
-
-  map(airtableCustomers, (airtableCustomer) => {
-    dataset[airtableCustomer.id] = { ...airtableCustomer, __typename: 'Customer' };
-  });
-
-  map(airtableThemes, (airtableTheme) => {
-    dataset[airtableTheme.id] = { ...airtableTheme, __typename: 'Theme' };
-  });
-
-  map(airtableProducts, (airtableProduct) => {
-    dataset[airtableProduct.id] = { ...airtableProduct, __typename: 'Product' };
-  });
+  const dataset: AirtableDataset = buildDataset([
+    { records: airtableCustomers, __typename: 'Customer' },
+    { records: airtableThemes, __typename: 'Theme' },
+    { records: airtableProducts, __typename: 'Product' },
+  ]);
 
   const airtableCustomer = find(airtableCustomers, { fields: { ref: customerRef } });
 
