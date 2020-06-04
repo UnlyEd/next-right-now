de993453275309751d33bc5173af116ad6109b50
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-06-04 21:06:56 +0200

    Misc remove dead code

--- src/pages/[locale]/examples/index.tsx
+++ src/pages/[locale]/examples/index.tsx
@@ -7,6 +7,7 @@ import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import React from 'react';
 import BuiltInFeaturesSection from '../../../components/doc/BuiltInFeaturesSection';
 import BuiltInUtilitiesSection from '../../../components/doc/BuiltInUtilitiesSection';
+import ExternalFeaturesSection from '../../../components/doc/ExternalFeaturesSection';
 import IntroductionSection from '../../../components/doc/IntroductionSection';
 import NativeFeaturesSection from '../../../components/doc/NativeFeaturesSection';
 import DefaultLayout from '../../../components/pageLayouts/DefaultLayout';
@@ -15,7 +16,6 @@ import { CommonServerSideParams } from '../../../types/nextjs/CommonServerSidePa
 import { OnlyBrowserPageProps } from '../../../types/pageProps/OnlyBrowserPageProps';
 import { SSGPageProps } from '../../../types/pageProps/SSGPageProps';
 import { getExamplesCommonStaticPaths, getExamplesCommonStaticProps } from '../../../utils/nextjs/SSG';
-import ExternalFeaturesSection from '../../../components/doc/ExternalFeaturesSection';
 
 const fileLabel = 'pages/[locale]/examples/index';
 const logger = createLogger({ // eslint-disable-line no-unused-vars,@typescript-eslint/no-unused-vars
@@ -71,119 +71,6 @@ const ExampleHomePage: NextPage<Props> = (props): JSX.Element => {
                 <BuiltInUtilitiesSection />
                 <ExternalFeaturesSection />
               </>
-              //   <div>
-              //
-              //     <div>
-              //       <div>
-              //         <h1>Overview</h1>
-              //         You can navigate between <I18nLink href={'/examples'}>/examples</I18nLink> and{' '}
-              //         <I18nLink
-              //           href={'/'}
-              //         >
-              //           <a>/</a>
-              //         </I18nLink> to see CSR in action.<br />
-              //         You can also disable JS on your browser to see how SSR works.<br />
-              //         <br />
-              //         If you want to know a bit more about what's running this demo beneath the surface, check out our <a href={'/api/status'}><code>/api/status</code> endpoint!</a>
-              //       </div>
-              //
-              //       <div>
-              //         <h3>Analytics overview</h3>
-              //         For the purpose of this demo, we are tracking your analytics usage.
-              //         <br />
-              //         For instance, we know if you've clicked on any link above. That's just basic analytics but it works out-of-the-box.
-              //         <br />
-              //
-              //         You can use
-              //         <a
-              //           href={'https://chrome.google.com/webstore/detail/amplitude-instrumentation/acehfjhnmhbmgkedjmjlobpgdicnhkbp'}
-              //           target={'_blank'}
-              //           rel={'noopener'}
-              //         > Amplitude Instrumentation Explorer extension
-              //         </a> to see what analytic events are sent and what they contains exactly, it's very powerful!<br />
-              //         <br />
-              //         Every time you visit a page, analytics are automatically sent (similar to Google Analytics "pageviews").<br />
-              //       </div>
-              //
-              //       <div>
-              //         <h3>GraphQL API overview</h3>
-              //         We fetched our GraphQL endpoint to display proper theming.<br />
-              //         The colors that are used in this demo are defined within the GraphCMS "Customer" model.
-              //       </div>
-              //
-              //       <div>
-              //         <h3>Monitoring overview</h3>
-              //         Any runtime error is configured to be sent to Sentry, which redirects it into our Slack channel.<br />
-              //         This allows us to be notified in real-time about anything that'd go wrong.<br />
-              //         <br />
-              //         You can test this behaviour by hitting <a href="/api/error"><code>/api/error</code> our error endpoint</a>. Don't worry, alerts have been disabled so you won't bother us for real ;)
-              //       </div>
-              //
-              //       <div>
-              //         <h3>I18n overview</h3>
-              //         You can change the language by clicking on the footer flag icon below. <br />
-              //         Changing language refresh the whole page, because it was just simpler to do that instead of running the GraphQL query again. <br />
-              //         But you could implement it without refreshing the whole page if you wanted.
-              //         <br />
-              //       </div>
-              //
-              //       <div>
-              //         <h3>Examples</h3>
-              //         Check out our
-              //         <I18nLink
-              //           href={'/examples'}
-              //         ><a>examples</a></I18nLink> to learn more and see some code snippets!<br />
-              //       </div>
-              //
-              //       <div>
-              //         <h3>Admin site</h3>
-              //         Check out our&nbsp;
-              //         <a
-              //           href={'https://nrn-admin.now.sh'}
-              //           target={'_blank'}
-              //           rel={'noopener'}
-              //         >Admin site</a> to edit the data that belong to the customer!<br />
-              //         Please do not use NSFW content or anything that is illegal as we don't enforce any rule. Everybody can change pics and text.<br />
-              //         <br />
-              //         The admin site is based on
-              //         <a
-              //           href={'https://github.com/marmelab/react-admin'}
-              //           target={'_blank'}
-              //           rel={'noopener'}
-              //         >react-admin</a>.<br />
-              //         The source code&nbsp;
-              //         <a
-              //           href={'https://github.com/UnlyEd/next-right-now-admin'}
-              //           target={'_blank'}
-              //           rel={'noopener'}
-              //         >is available on GitHub</a> as well. <br />
-              //         It also relies on&nbsp;
-              //         <a
-              //           href={'https://github.com/UnlyEd/ra-data-graphql-prisma'}
-              //           target={'_blank'}
-              //           rel={'noopener'}
-              //         >our open source data provider</a> for react-admin, using GraphQL.<br />
-              //         <br />
-              //
-              //         All the admin site (AKA back-office) uses GraphQL schema definition to build the views and GQL queries/mutations. (but allow override for flexibility)<br />
-              //         It's a POC and could use the help of the community. I've started it to build our Back-office but found a better alternative in the meantime that better answers our needs:&nbsp;
-              //         <a
-              //           href={'https://directus.io/'}
-              //           target={'_blank'}
-              //           rel={'noopener'}
-              //         >Directus</a><br />
-              //         So, I won't likely bring NRN-Admin to a production-grade level, and it will likely stay in it's current state: a POC.
-              //       </div>
-              //
-              //       <div>
-              //         <Alert color={'success'}>
-              //           Feel free to ask for more examples of what this demo can offer by creating an issue on Github! :)<br />
-              //           Feel free to make an improvement to this demo as well, though a PR. (if it's big, please let's discuss it first!)
-              //         </Alert>
-              //       </div>
-              //     </div>
-              //   </div>
-              // </Container>
             );
           }
         }
