9ed771157325bce08729294236245256eb304732
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-06-04 02:34:55 +0200

    Fix TS typing

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
-import { Theme } from './types/data/Theme';
+import { CustomerTheme } from './types/data/CustomerTheme';
 
 export const NRN_DEFAULT_SERVICE_LABEL = 'Next Right Now!';
 export const NRN_DEFAULT_FONT = 'neuzeit-grotesk';
 export const NRN_DEFAULT_SECONDARY_COLOR = '#fff';
 
-export const NRN_DEFAULT_THEME: Theme = {
+export const NRN_DEFAULT_THEME: CustomerTheme = {
   primaryColor: 'blue',
 };
