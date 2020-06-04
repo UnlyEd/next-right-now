90e04ece4755942a91314545a24febb1d4b0f72c
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-06-04 11:46:06 +0200

    Fix jest config setupFilesAfterEnv (conflict between package.json:jest and jest.config.js) - See https://github.com/facebook/jest/issues/10123

--- src/utils/api/fetchAirtable.test.ts
+++ src/utils/api/fetchAirtable.test.ts
@@ -1,13 +1,36 @@
-import fetchAirtableTable from './fetchAirtableTable';
+import map from 'lodash.map';
+import { CUSTOMER1 } from '../../mocks/airtableDataset';
+import { AirtableRecord } from '../../types/data/AirtableRecord';
+import { Customer } from '../../types/data/Customer';
+import fetchAirtableTable, { GenericListApiResponse } from './fetchAirtableTable';
 
-// TODO "fetch" is not found here - See https://github.com/vercel/next.js/discussions/13678
-// Skipped until resolved
-describe.skip(`utils/api/fetchAirtable.ts`, () => {
-  const results = {};
+describe(`utils/api/fetchAirtable.ts`, () => {
+  const expectedResults = {
+      records: [
+        CUSTOMER1,
+      ],
+    }
+  ;
   describe(`fetchAirtableTable`, () => {
     describe(`should fetch correctly`, () => {
       test(`when not using any option`, async () => {
-        expect(await fetchAirtableTable('Customer')).toMatchObject(results);
+        const results = await fetchAirtableTable<GenericListApiResponse<AirtableRecord<Customer>>>('Customer');
+
+        // Make some fields optional
+        // Awaiting better implementation - See https://github.com/Dean177/jest-to-match-shape-of/issues/75
+        map(results, (result: AirtableRecord<Customer>, i: number) => {
+          results[i] = {
+            ...result,
+            fields: {
+              labelEN: '',
+              labelFR: '',
+              termsEN: '',
+              termsFR: '',
+            },
+          };
+        });
+
+        expect(results).toMatchShapeOf(expectedResults);
       });
     });
   });
