56185daea98d90385566ad982e7fb8d31fc6979c
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-06-14 15:09:11 +0200

    Add API (airtable) documentation

--- src/pages/[locale]/examples/built-in-features/api.tsx
+++ src/pages/[locale]/examples/built-in-features/api.tsx
@@ -9,6 +9,7 @@ import BuiltInFeaturesSidebar from '../../../../components/doc/BuiltInFeaturesSi
 import DocPage from '../../../../components/doc/DocPage';
 import DefaultLayout from '../../../../components/pageLayouts/DefaultLayout';
 import Code from '../../../../components/utils/Code';
+import ExternalLink from '../../../../components/utils/ExternalLink';
 import { CommonServerSideParams } from '../../../../types/nextjs/CommonServerSideParams';
 import { OnlyBrowserPageProps } from '../../../../types/pageProps/OnlyBrowserPageProps';
 import { SSGPageProps } from '../../../../types/pageProps/SSGPageProps';
@@ -58,18 +59,50 @@ const ExampleApiPage: NextPage<Props> = (props): JSX.Element => {
       <DocPage>
         <h1 className={'pcolor'}>API examples, using Airtable vendor</h1>
 
-        <Alert color={'warning'}>
-
+        <Alert color={'info'}>
+          We wrote a helper <code>fetchCustomer</code> that handles a lot of things.<br />
+          It basically reconciliates all data that belongs to a customer and resolved nested deps. <br />
+          It also handles i18n fields and resolves the best value to use based on the wanted locale and available translations.<br />
         </Alert>
 
         <Code
           text={`
-
+            // Fetches all Airtable tables and returns a consolidated Customer object with all relations resolved
+            // Relations are only resolved on the two first levels (to avoid circular dependencies)
+            const customer: AirtableRecord<Customer> = await fetchCustomer(bestCountryCodes);
           `}
         />
 
+        <br />
+
         <p>
           The above code is what we actually use in <code>getCommonStaticProps</code>, to fetch data that are shared by all SSG pages.
+        </p>
+
+        <hr />
+
+        <Alert color={'info'}>
+          We use a cache using disk storage during build time (for SSG pages), so that each page doesn't have to fetch the Airtable API.<br />
+          This is very important when generating static pages, otherwise we end up making hundreds of API calls (3 calls per page, because we fetch 3 tables). <br />
+          Storing cache in memory doesn't work, because each page generation is executed in different processes (or so) and don't share the same memory (RAM).<br />
+          Creating files like this requires a writable disk,
+          <ExternalLink href={'https://github.com/vercel/next.js/discussions/13765#discussioncomment-22703'} suffix={null}>which is the case with Vercel</ExternalLink>.
+        </Alert>
+
+        <Code
+          text={`
+            const customerRef = process.env.NEXT_PUBLIC_CUSTOMER_REF;
+            const { records: airtableCustomers } = await hybridCache('CustomerTable', async () => await fetchAirtableTable<GenericListApiResponse<AirtableRecord<Customer>>>('Customer'), {
+              enabled: !!process.env.IS_SERVER_INITIAL_BUILD && process.env.NODE_ENV !== 'development',
+              storage: { type: 'disk', options: { filename: 'CustomerTable' } },
+            });
+          `}
+        />
+
+        <br />
+
+        <p>
+          The above code is part of what we actually use in <code>fetchCustomer</code>, to fetch data from all tables.
         </p>
 
       </DocPage>
