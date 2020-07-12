c3206476f606952ddc5e1d370359fb67b7f975a7
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-07-13 00:26:21 +0200

    Add ore E2E tests for both SSG/SSR

--- cypress/integration/app/common/nav.js
+++ cypress/integration/app/common/nav.js
@@ -13,7 +13,13 @@ describe('Common > Nav section', () => {
   });
 
   it('should have a link in the navbar that redirects to the home page', () => {
-    cy.url().should('eq', `${baseUrl}/en`);
+    cy.get('#nav-link-home')
+      .should('have.text', 'Home')
+      .click();
+    cy.url({ timeout: 10000 }).should('eq', `${baseUrl}/en`);
+  });
+
+  it('should have a link in the navbar that redirects to the built-in feature "Static I18n"', () => {
     cy.get('#nav-link-examples')
       .should('have.text', 'Examples')
       .click();
@@ -21,5 +27,17 @@ describe('Common > Nav section', () => {
       .should('have.text', 'Static i18n')
       .click();
     cy.url({ timeout: 10000 }).should('eq', `${baseUrl}/en/examples/built-in-features/static-i18n`);
+    cy.get('h1').should('have.length', 1).should('have.text', 'Static i18n examples, using i18next and Locize vendor');
+  });
+
+  it('should have a link in the navbar that redirects to the native feature "SSR"', () => {
+    cy.get('#nav-link-examples')
+      .should('have.text', 'Examples')
+      .click();
+    cy.get('#nav-link-examples-ssr-get-server-side-props')
+      .should('have.text', 'SSR (getServerSideProps)')
+      .click();
+    cy.url({ timeout: 10000 }).should('eq', `${baseUrl}/en/examples/native-features/example-with-ssr`);
+    cy.get('h1').should('have.length', 1).should('have.text', 'Example, using SSR');
   });
 });
