483e31868434b94738dafd9c0f3788c43cb5cc31
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-07-16 03:10:14 +0200

    Add i18n translation for sentences in PreviewModeBanner component

--- src/components/pageLayouts/PreviewModeBanner.tsx
+++ src/components/pageLayouts/PreviewModeBanner.tsx
@@ -3,6 +3,7 @@ import { css, jsx } from '@emotion/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NextRouter, useRouter } from 'next/router';
 import React from 'react';
+import { Trans, useTranslation } from 'react-i18next';
 import { Button } from 'reactstrap';
 import Alert from 'reactstrap/lib/Alert';
 import usePreviewMode, { PreviewMode } from '../../hooks/usePreviewMode';
@@ -15,16 +16,22 @@ type Props = {}
 
 const ExplanationTooltipOverlay: React.FunctionComponent = (): JSX.Element => {
   return (
-    <span>
-      When <b>preview mode</b> is enabled, SSG is by-passed and all pages behave as if they were using SSR.<br />
-      It's a great feature when you want to preview content on staging without having to rebuild your whole application.<br />
-      <ExternalLink href={'https://nextjs.org/docs/advanced-features/preview-mode'}>Learn more</ExternalLink><br />
-      <br />
-      Disabling <b>preview mode</b> will make SSG pages go back to their normal behaviour, but we decided to always enforce it in the staging stage (force opt-in).<br />
-      <br />
-      We also decided the preview mode won't be enabled in the production stage, because we want to preview content only in staging, and see the real data in production.<br />
-      This is a choice, it could be implemented differently, but it's what makes the most sense for us, considering our publication workflow.
-    </span>
+    <Trans
+      i18nKey={'previewModeBanner.previewModeTitleHelp'}
+    >
+      <span>
+        When the Next.js <b>preview mode</b> is enabled, SSG is by-passed and all pages behave as if they were using SSR.<br />
+        It's a great feature when you want to preview content on staging without having to rebuild your whole application.<br />
+        <ExternalLink href={'https://nextjs.org/docs/advanced-features/preview-mode'}>Learn more</ExternalLink><br />
+        <br />
+        Disabling <b>preview mode</b> will make SSG pages go back to their normal behaviour, but we decided to always enforce it in the staging stage (force opt-in).<br />
+        <br />
+        We also decided the preview mode won't be enabled in the production stage, because we want to preview content only in staging, and see the real data in production.<br />
+        This is a choice, it could be implemented differently, but it's what makes the most sense to us, considering our publication workflow.<br />
+        <br />
+        On SSR pages, the <b>preview mode</b> has no effect and is being ignored.
+      </span>
+    </Trans>
   );
 };
 
@@ -41,6 +48,7 @@ const PreviewModeBanner: React.FunctionComponent<Props> = (props): JSX.Element =
   const { preview }: PreviewMode = usePreviewMode();
   const router: NextRouter = useRouter();
   const queryParameters: string = stringifyQueryParameters(router);
+  const { t } = useTranslation();
 
   if (process.env.NEXT_PUBLIC_APP_STAGE === 'production') {
     return null;
@@ -80,7 +88,8 @@ const PreviewModeBanner: React.FunctionComponent<Props> = (props): JSX.Element =
         preview ? (
           <div>
             <span>
-              You are using the preview environment.
+              {t(`previewModeBanner.previewModeEnabledTitle`, `Vous êtes sur l'environnement de prévisualisation`)}
+              &nbsp;
               <Tooltip
                 overlay={<ExplanationTooltipOverlay />}
                 placement={'bottom'}
@@ -97,7 +106,7 @@ const PreviewModeBanner: React.FunctionComponent<Props> = (props): JSX.Element =
                     onClick={(): void => stopPreviewMode(queryParameters)}
                     onKeyPress={(): void => stopPreviewMode(queryParameters)}
                   >
-                    Leave preview mode
+                    {t(`previewModeBanner.stopPreviewMode`, `Stopper l'environnement de prévisualisation`)}
                   </Button>
                 </span>
               )
@@ -106,7 +115,8 @@ const PreviewModeBanner: React.FunctionComponent<Props> = (props): JSX.Element =
         ) : (
           <div>
             <span>
-              Preview mode is disabled&nbsp;
+              {t(`previewModeBanner.previewModeDisabledTitle`, `L'environnement de prévisualisation est désactivé`)}
+              &nbsp;
               <Tooltip
                 overlay={<ExplanationTooltipOverlay />}
                 placement={'bottom'}
@@ -123,7 +133,7 @@ const PreviewModeBanner: React.FunctionComponent<Props> = (props): JSX.Element =
                     onClick={(): void => startPreviewMode(queryParameters)}
                     onKeyPress={(): void => startPreviewMode(queryParameters)}
                   >
-                    Start preview mode
+                    {t(`previewModeBanner.startPreviewMode`, `Démarrer l'environnement de prévisualisation`)}
                   </Button>
                 </span>
               )
