b45c1a68d41e90d62eef93a09e55ad1c22833c44
Vadorequest <ambroise.dhenain@gmail.com>
2020-11-21 15:35:46 +0100

    Improve LightHouse performances (#214) (modified)

---
+++ src/utils/quality/lighthouse.ts
@@ -0,0 +1,12 @@
+/**
+ * Detects whether LightHouse is running on the page or not.
+ *
+ * Returns null when not executed from the browser.
+ */
+export const detectLightHouse = (): boolean | null => {
+  if (typeof window) {
+    return navigator?.userAgent?.indexOf('Chrome-Lighthouse') > -1;
+  }
+
+  return null;
+};
