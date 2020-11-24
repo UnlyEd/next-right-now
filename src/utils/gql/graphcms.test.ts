2ebbf6692677a3726987f623d8db06cf977934ad
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-11-24 20:03:51 +0100

    Fix tests

--- src/utils/gql/graphcms.test.ts
+++ src/utils/gql/graphcms.test.ts
@@ -4,15 +4,18 @@ describe(`utils/graphcms.ts`, () => {
   describe(`prepareGraphCMSLocaleHeader`, () => {
     describe(`should clean properly the header locale`, () => {
       test(`when using 1 language`, async () => {
-        expect(prepareGraphCMSLocaleHeader(['fr'])).toEqual(`FR`);
+        expect(prepareGraphCMSLocaleHeader(['fr'])).toEqual(`fr`);
+        expect(prepareGraphCMSLocaleHeader(['FR'])).toEqual(`fr`);
       });
 
       test(`when using 2 languages`, async () => {
-        expect(prepareGraphCMSLocaleHeader(['fr', 'en'])).toEqual(`FR, EN`);
+        expect(prepareGraphCMSLocaleHeader(['fr', 'en'])).toEqual(`fr, en`);
+        expect(prepareGraphCMSLocaleHeader(['FR', 'EN'])).toEqual(`fr, en`);
       });
 
       test(`when using 3 languages`, async () => {
-        expect(prepareGraphCMSLocaleHeader(['fr', 'en', 'es'])).toEqual(`FR, EN, ES`);
+        expect(prepareGraphCMSLocaleHeader(['fr', 'en', 'es'])).toEqual(`fr, en, es`);
+        expect(prepareGraphCMSLocaleHeader(['FR', 'EN', 'ES'])).toEqual(`fr, en, es`);
       });
     });
   });
