9f719195afa0c89316d0b365779153da207de726
Hugo MARTIN <hugo.martin@epitech.eu>
2020-11-26 18:07:53 +0100

    Error management

--- src/pages/api/deployToProduction.ts
+++ src/pages/api/deployToProduction.ts
@@ -12,45 +12,46 @@ const logger = createLogger({
 });
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  const PRODUCTION_CI_FILE = '.github/workflows/deploy-vercel-production.yml';
-  const deployRef = req.query.ref || process.env.NEXT_PUBLIC_NRN_PRESET;
+  if (!process.env.GITHUB_DISPATCH_TOKEN) {
+    res.send('Cannot find Github token, please fill it in .env');
+  } else {
+    const PRODUCTION_CI_FILE = '.github/workflows/deploy-vercel-production.yml';
+    const deployRef = req.query.ref || process.env.NEXT_PUBLIC_NRN_PRESET;
 
-  try {
-    configureReq(req);
+    try {
+      configureReq(req);
 
-    console.log('Github token: ', process.env.GITHUB_DISPATCH_TOKEN);
-    console.log('Customer: ', process.env.NEXT_PUBLIC_CUSTOMER_REF);
-
-    fetch('https://api.github.com/repos/UnlyEd/next-right-now/actions/workflows').then((plainData) => plainData.json()).then((data) => {
-      const [productionWorkflow] = data.workflows.filter((workflow) => workflow.path === PRODUCTION_CI_FILE);
-      if (productionWorkflow) {
-        fetch(`${productionWorkflow.url}/dispatches`, {
-          method: 'POST',
-          headers: {
-            Authorization: `token ${process.env.GITHUB_DISPATCH_TOKEN}`,
-            Accept: 'application/vnd.github.v3+json'
-          },
-          body: JSON.stringify({
-            inputs: {
-              customer: process.env.NEXT_PUBLIC_CUSTOMER_REF
+      fetch('https://api.github.com/repos/UnlyEd/next-right-now/actions/workflows').then((plainData) => plainData.json()).then((data) => {
+        const [productionWorkflow] = data.workflows.filter((workflow) => workflow.path === PRODUCTION_CI_FILE);
+        if (productionWorkflow) {
+          fetch(`${productionWorkflow.url}/dispatches`, {
+            method: 'POST',
+            headers: {
+              Authorization: `token ${process.env.GITHUB_DISPATCH_TOKEN}`,
+              Accept: 'application/vnd.github.v3+json'
             },
-            ref: deployRef
-          })
-        });
-      }
-    });
-    res.send('Action started');
-  } catch (e) {
-    logger.error(e.message);
+            body: JSON.stringify({
+              inputs: {
+                customer: process.env.NEXT_PUBLIC_CUSTOMER_REF
+              },
+              ref: deployRef
+            })
+          });
+        }
+      });
+      res.send('Action started');
+    } catch (e) {
+      logger.error(e.message);
 
-    Sentry.withScope((scope): void => {
-      scope.setTag('fileLabel', fileLabel);
-      Sentry.captureException(e);
-    });
+      Sentry.withScope((scope): void => {
+        scope.setTag('fileLabel', fileLabel);
+        Sentry.captureException(e);
+      });
 
-    res.json({
-      error: true,
-      message: process.env.NEXT_PUBLIC_APP_STAGE === 'production' ? undefined : e.message,
-    });
+      res.json({
+        error: true,
+        message: process.env.NEXT_PUBLIC_APP_STAGE === 'production' ? undefined : e.message,
+      });
+    }
   }
 };
