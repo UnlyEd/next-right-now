41e75b6035422812a851e4b25250c9dcd3093c80
Hugo MARTIN <hugo.martin@epitech.eu>
2020-11-26 18:01:05 +0100

    Message asap

--- src/pages/api/deployToProduction.ts
+++ src/pages/api/deployToProduction.ts
@@ -17,30 +17,29 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
 
   try {
     configureReq(req);
-    
+
     console.log('Github token: ', process.env.GITHUB_DISPATCH_TOKEN);
     console.log('Customer: ', process.env.NEXT_PUBLIC_CUSTOMER_REF);
 
     fetch('https://api.github.com/repos/UnlyEd/next-right-now/actions/workflows').then((plainData) => plainData.json()).then((data) => {
       const [productionWorkflow] = data.workflows.filter((workflow) => workflow.path === PRODUCTION_CI_FILE);
-      fetch(`${productionWorkflow.url}/dispatches`, {
-        method: 'POST',
-        headers: {
-          Authorization: `token ${process.env.GITHUB_DISPATCH_TOKEN}`,
-          Accept: 'application/vnd.github.v3+json'
-        },
-        body: JSON.stringify({
-          inputs: {
-            customer: process.env.NEXT_PUBLIC_CUSTOMER_REF
+      if (productionWorkflow) {
+        fetch(`${productionWorkflow.url}/dispatches`, {
+          method: 'POST',
+          headers: {
+            Authorization: `token ${process.env.GITHUB_DISPATCH_TOKEN}`,
+            Accept: 'application/vnd.github.v3+json'
           },
-          ref: deployRef
-        })
-      }).then(async (response) => {
-        console.log('response: ', response.status);
-      });
+          body: JSON.stringify({
+            inputs: {
+              customer: process.env.NEXT_PUBLIC_CUSTOMER_REF
+            },
+            ref: deployRef
+          })
+        });
+      }
     });
-
-    res.json('Hello World !');
+    res.send('Action started');
   } catch (e) {
     logger.error(e.message);
 
