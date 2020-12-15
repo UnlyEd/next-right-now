c37f59f39697486ca69a376705a0277b5282ac99
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-12-15 16:48:35 +0100

    Protect / only, but will likely not improve things

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const protect = require('static-auth');
 const safeCompare = require('safe-compare');
 
 const app = protect(
-  '/admin',
+  '/',
   (username, password) => safeCompare(username, 'admin') && safeCompare(password, 'admin'),
 );
 
