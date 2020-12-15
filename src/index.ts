47e57e3f7a65d12864af713983f263563f36b2c7
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-12-15 16:22:00 +0100

    Remove advanced config

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,6 @@ import protect from 'static-auth';
 const app = protect(
   '/',
   (username, password) => safeCompare(username, 'admin') && safeCompare(password, 'admin'),
-  {
-    directory: __dirname + '/_static',
-    realm: 'vercel-basic-auth.node-static-auth',
-    onAuthFailed: (res) => {
-      res.end('Restricted area, please login (admin:admin).');
-    },
-  },
 );
 
 export default app;
