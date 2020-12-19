9876d0337b6bc2810b65308849641ecce1b0ae44
Hugo MARTIN <hugo.martin@epitech.eu>
2020-12-19 22:39:49 +0100

    Using new script to export git variables

---
+++ scripts/populate-git-env.sh
@@ -0,0 +1,7 @@
+# List of environment variables you want to have access during next development and deployment.
+# To have access to your variables (e.g: echo $GIT_COMMIT_SHA) you have to run this script like this: `. scripts/populate-git-env.sh`
+# If you miss the dot or use a prefix like sh it's not going to work.
+
+export "GIT_COMMIT_TAGS=${GIT_COMMIT_TAGS:-$(yarn --silent git:getReleasesAndTags)}"
+export "GIT_COMMIT_REF=${GIT_COMMIT_REF:-$(yarn --silent git:getCommitRef)}"
+export "GIT_COMMIT_SHA=${GIT_COMMIT_SHA:-$(yarn --silent git:getCommitSHA)}"
