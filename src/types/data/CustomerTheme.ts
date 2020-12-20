062cd9c0088a6ea9064e742b57f157d1ff867a04
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-12-20 22:49:42 +0100

    Misc doc

--- src/types/data/CustomerTheme.ts
+++ src/types/data/CustomerTheme.ts
@@ -5,5 +5,9 @@ import { Theme } from './Theme';
  *
  * CustomerTheme is what's really used for theming, and doesn't include useless properties from the Theme entity.
  * Also, all properties defined in Theme will be set in CustomerTheme, because fallback values will have been applied.
+ *
+ * Inspired by MaterialUI Design system.
+ *
+ * @see https://material.io/design/color/the-color-system.html#color-theme-creation
  */
 export type CustomerTheme = Required<Omit<Theme, 'ref' | 'id' | '__typename'>> & {};
