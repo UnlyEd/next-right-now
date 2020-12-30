72b3ece2c05bdf44ff1be34241c39a5a968e0b52
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-12-30 19:01:17 +0100

    Add appBuildTimeISO example in /api/status

--- src/pages/api/status.ts
+++ src/pages/api/status.ts
@@ -31,6 +31,7 @@ export const status = async (req: NextApiRequest, res: NextApiResponse): Promise
       appName: process.env.NEXT_PUBLIC_APP_NAME,
       appRelease: process.env.NEXT_PUBLIC_APP_VERSION_RELEASE,
       appBuildTime: process.env.NEXT_PUBLIC_APP_BUILD_TIME,
+      appBuildTimeISO: (new Date(process.env.NEXT_PUBLIC_APP_BUILD_TIME)).toISOString(),
       appBuildTimestamp: process.env.NEXT_PUBLIC_APP_BUILD_TIMESTAMP,
       appBuildId: process.env.NEXT_PUBLIC_APP_BUILD_ID,
       nodejs: process.version,
