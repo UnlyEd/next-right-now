9deda83f4dfc8ce629a7fb03df8bc2c04cfd32e0
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-12-30 21:48:01 +0100

    Fix test by using null if NEXT_PUBLIC_APP_BUILD_TIME is undefined

--- src/pages/api/status.ts
+++ src/pages/api/status.ts
@@ -31,7 +31,7 @@ export const status = async (req: NextApiRequest, res: NextApiResponse): Promise
       appName: process.env.NEXT_PUBLIC_APP_NAME,
       appRelease: process.env.NEXT_PUBLIC_APP_VERSION_RELEASE,
       appBuildTime: process.env.NEXT_PUBLIC_APP_BUILD_TIME,
-      appBuildTimeISO: (new Date(process.env.NEXT_PUBLIC_APP_BUILD_TIME)).toISOString(),
+      appBuildTimeISO: (new Date(process.env.NEXT_PUBLIC_APP_BUILD_TIME || null)).toISOString(),
       appBuildTimestamp: process.env.NEXT_PUBLIC_APP_BUILD_TIMESTAMP,
       appBuildId: process.env.NEXT_PUBLIC_APP_BUILD_ID,
       nodejs: process.version,
