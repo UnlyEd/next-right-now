1d0d94e24c32e1dc5d481d9e880d311b54cc1546
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-01-10 20:34:47 +0100

    Misc typo

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
   testEnvironment: 'node',
   globals: {
     // XXX we must specify a custom tsconfig for tests because we need the typescript transform
-    //  to transform jsx into js rather than leaving it jsx such as the next build requires.  you
-    //  can see this setting in tsconfig.jest.json -> "jsx": "react"
+    //  to transform jsx into js rather than leaving it jsx such as the next build requires.
+    //  You can see this setting in tsconfig.jest.json -> "jsx": "react"
     //  See https://github.com/vercel/next.js/issues/8663
     'ts-jest': {
       tsconfig: 'tsconfig.jest.json',
