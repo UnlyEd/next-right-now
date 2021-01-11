15345fdea1fd06900a07588e83f6a590b423ed34
Vadorequest <ambroise.dhenain@gmail.com>
2021-01-11 09:32:56 +0100

    Deps - Upgrade `v2-mst-aptd-at-lcz-sty` packages (January 2021) (attempt N°2) (#245)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,25 +15,7 @@ import { sendWebVitals } from '../utils/analytics/amplitude';
 import '../utils/app/ignoreNoisyWarningsHacks'; // HACK This ignore warnings and errors I personally find too noisy and useless
 import '../utils/icons/font-awesome';
 import '../utils/monitoring/sentry';
-
-/**
- * WDYR (why-did-you-render) helps locate unnecessary re-renders and fix them
- * Applied in development environment, on the frontend only
- *
- * @see https://github.com/welldone-software/why-did-you-render
- */
-if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  // eslint-disable-next-line no-console
-  console.debug('Applying whyDidYouRender, to help you locate unnecessary re-renders during development. See https://github.com/welldone-software/why-did-you-render');
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-    trackHooks: true,
-    logOwnerReasons: true,
-    collapseGroups: true,
-  });
-}
+import '../utils/wdyr';
 
 /**
  * "props.pageProps" will depend on whether the page is served by server or client, SSG or SSR
