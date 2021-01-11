d48c0da810b3f3cc34feb9525821b8eba00bee66
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-01-12 00:48:34 +0100

    Add storybook top-level decorators (simulate Next Right Now _app & App Bootstrap) + add withNextRouter

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,44 @@
 // Import the same 3rd party libraries global styles as the pages/_app.tsx (for UI consistency)
+import { Amplitude, AmplitudeProvider } from '@amplitude/react-amplitude';
+import { ThemeProvider } from '@emotion/react';
+import { addDecorator } from '@storybook/react';
+import find from 'lodash.find';
+import React from 'react';
+import { withNextRouter } from 'storybook-addon-next-router';
 import '../src/components/appBootstrap/MultiversalGlobalExternalStyles';
-import '../src/components/appBootstrap/MultiversalGlobalStyles';
+import MultiversalGlobalStyles from '../src/components/appBootstrap/MultiversalGlobalStyles';
+import amplitudeContext from '../src/stores/amplitudeContext';
+import customerContext from '../src/stores/customerContext';
+import { cypressContext } from '../src/stores/cypressContext';
+import datasetContext from '../src/stores/datasetContext';
+import i18nContext from '../src/stores/i18nContext';
+import previewModeContext from '../src/stores/previewModeContext';
+import quickPreviewContext from '../src/stores/quickPreviewContext';
+import userConsentContext from '../src/stores/userConsentContext';
+import { userSessionContext } from '../src/stores/userSessionContext';
+import { getAmplitudeInstance } from '../src/utils/analytics/amplitude';
+import { initCustomerTheme } from '../src/utils/data/theme';
+import dataset from './mock/sb-dataset';
+
+const customer = find(dataset, { __typename: 'Customer' });
+const customerTheme = initCustomerTheme(customer);
+const customerRef = 'storybook';
+const lang = 'en';
+const locale = 'en';
+const userConsent = {
+  isUserOptedOutOfAnalytics: true,
+  hasUserGivenAnyCookieConsent: false,
+};
+const userId = 'storybook';
+const amplitudeInstance = getAmplitudeInstance({
+  customerRef,
+  iframeReferrer: null,
+  isInIframe: false,
+  lang,
+  locale,
+  userId,
+  userConsent: userConsent,
+});
 
 /**
  * Story Global parameters for Storybook.
@@ -27,3 +65,89 @@ export const parameters = {
     },
   },
 };
+
+/**
+ * Allow to use Next.js Router in Storybook stories.
+ *
+ * @see https://github.com/lifeiscontent/storybook-addon-next-router#usage-in-previewjs
+ */
+addDecorator(
+  withNextRouter({
+    path: '/', // defaults to `/`
+    asPath: '/', // defaults to `/`
+    query: {}, // defaults to `{}`
+    push() {}, // defaults to using addon actions integration, can override any method in the router
+  }),
+);
+
+/**
+ * Decorators in .storybook/preview.js are used for context mocking.
+ *
+ * Basically, they play a similar role to _app and appBootstrap components (MultiversalAppBootstrap, etc.)
+ *
+ * @type {(function(*))[]}
+ * @see https://storybook.js.org/docs/react/writing-stories/decorators#context-for-mocking
+ */
+export const decorators = [
+  (Story) => (
+    <datasetContext.Provider value={dataset}>
+      <quickPreviewContext.Provider value={false}>
+        <previewModeContext.Provider
+          value={{
+            isPreviewModeEnabled: false,
+            previewData: null,
+          }}
+        >
+          <i18nContext.Provider
+            value={{
+              lang: lang,
+              locale: locale,
+            }}
+          >
+            <customerContext.Provider value={customer}>
+              <MultiversalGlobalStyles customerTheme={customerTheme} />
+
+              <ThemeProvider theme={customerTheme}>
+                <AmplitudeProvider
+                  amplitudeInstance={amplitudeInstance}
+                  apiKey={process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY}
+                  userId={userId}
+                >
+                  <Amplitude
+                    eventProperties={{
+                      app: {
+                        name: customerRef,
+                        release: customerRef,
+                        stage: 'development',
+                      },
+                      page: {
+                        url: location.href,
+                        path: location.pathname,
+                        origin: location.origin,
+                      },
+                      customer: {
+                        ref: customerRef,
+                      },
+                      lang: lang,
+                      locale: locale,
+                    }}
+                  >
+                    <cypressContext.Provider value={false}>
+                      <amplitudeContext.Provider value={{ amplitudeInstance }}>
+                        <userSessionContext.Provider value={null}>
+                          <userConsentContext.Provider value={null}>
+                            <Story />
+                          </userConsentContext.Provider>
+                        </userSessionContext.Provider>
+                      </amplitudeContext.Provider>
+                    </cypressContext.Provider>
+                  </Amplitude>
+                </AmplitudeProvider>
+              </ThemeProvider>
+            </customerContext.Provider>
+          </i18nContext.Provider>
+        </previewModeContext.Provider>
+      </quickPreviewContext.Provider>
+    </datasetContext.Provider>
+  ),
+];
