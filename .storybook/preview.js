28de4ef501e91518eb05f14d1271f5b9c8620d1f
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-01-11 22:58:57 +0100

    Add empty skeletons for preview + doc

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,9 +7,13 @@ import '../src/components/appBootstrap/MultiversalGlobalExternalStyles';
  * Couldn't find a documentation reference for all options.
  *
  * @see https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
+ * @see https://github.com/storybookjs/storybook/blob/master/addons/actions/ADVANCED.md#configuration
+ * @see https://storybook.js.org/docs/react/essentials/backgrounds#configuration
  */
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: {
+    argTypesRegex: '^on[A-Z].*'
+  },
   options: {
     // See https://storybook.js.org/docs/react/writing-stories/naming-components-and-hierarchy#sorting-stories
     storySort: {
