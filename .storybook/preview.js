800bc7b8348e5b411a88a309d311d663f063a730
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-01-11 22:24:21 +0100

    Import MultiversalGlobalExternalStyles in all Storybook stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
+// Import the same 3rd party libraries global styles as the pages/_app.tsx (for UI consistency)
+import '../src/components/appBootstrap/MultiversalGlobalExternalStyles';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
