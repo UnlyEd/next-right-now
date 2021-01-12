86c78512c19a96c07ac0b2cb595afeb4f4519b64
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-01-12 01:08:08 +0100

    Change babel config for storybook (classic instead of automatic)

--- .storybook/babel.config.js
+++ .storybook/babel.config.js
@@ -4,17 +4,10 @@
  * Doesn't affect Next.js babel configuration, specific file for Storybook only.
  * Need to apply Emotion babel configuration, otherwise Emotion "css" cannot be used in Storybook.
  *
+ * XXX We use the "classic" way instead of the "automatic" way for Storybook, that's because MDX isn't compatible with "automatic".
+ *
  * @see https://emotion.sh/docs/css-prop#babel-preset Configuring Emotion 11
  */
 module.exports = {
-  "presets": [
-    [
-      "@babel/preset-react",
-      {
-        "runtime": "automatic",
-        "importSource": "@emotion/react"
-      }
-    ]
-  ],
-  "plugins": ["@emotion/babel-plugin"]
+  "presets": ["@emotion/babel-preset-css-prop"]
 };
