b79e94e05199f588c23f01914b47942b36e37e70
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-01-12 11:15:23 +0100

    Improve doc about GA

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,20 @@
 module.exports = {
-  'stories': [
+  stories: [
     '../src/**/*.stories.mdx',
     '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  'addons': [
+  addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+
+    /**
+     * We use Google Analytics for tracking analytics usage.
+     *
+     * It's much easier to setup than Amplitude, because there is an official dedicated plugin for this.
+     * See ".storybook/manager.js" for Google Analytics configuration.
+     *
+     * @see https://github.com/storybookjs/storybook/tree/master/addons/google-analytics
+     */
     '@storybook/addon-google-analytics',
   ],
 };
