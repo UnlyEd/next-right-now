bfdeb62d777e9dd6ef43f5d279f5c9a4ece646d7
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-01-12 11:55:03 +0100

    Improve documentation about Storybook addons being used

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,8 +4,60 @@ module.exports = {
     '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
+    /**
+     * The Storybook Links addon can be used to create links that navigate between stories in Storybook.
+     *
+     * @see https://www.npmjs.com/package/@storybook/addon-links
+     */
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+
+    /**
+     * Present including "essential" Storybook addons, such as:
+     *
+     * - Actions - The Toolbars addon controls global story rendering options from Storybook's toolbar UI.
+     *
+     * - Backgrounds - Storybook Addon Backgrounds can be used to change background colors inside the preview in Storybook.
+     *
+     * - Controls - Controls gives you a graphical UI to interact with a component's arguments dynamically, without needing to code.
+     *    It creates an addon panel next to your component examples ("stories"), so you can edit them live.
+     *
+     * - Docs - Storybook Docs transforms your Storybook stories into world-class component documentation.
+     *    - DocsPage: Out of the box, all your stories get a DocsPage. DocsPage is a zero-config aggregation
+     *      of your component stories, text descriptions, docgen comments, props tables, and code examples into clean, readable pages.
+     *    - MDX: If you want more control, MDX allows you to write long-form markdown documentation and stories in one file.
+     *      You can also use it to write pure documentation pages and embed them inside your Storybook alongside your stories.
+     *
+     * - Viewport - Storybook Viewport Addon allows your stories to be displayed in different sizes and layouts in Storybook.
+     *    This helps build responsive components inside of Storybook.
+     *
+     * - Toolbars - The Toolbars addon controls global story rendering options from Storybook's toolbar UI. It's a general purpose addon that can be used to:
+     *    - set a theme for your components
+     *    - set your components' internationalization (i18n) locale
+     *    - configure just about anything in Storybook that makes use of a global variable
+     *
+     * @see https://storybook.js.org/addons/essentials
+     * @see https://github.com/storybookjs/storybook/tree/master/addons/essentials
+     * @see https://github.com/storybookjs/storybook/tree/next/addons/actions
+     * @see https://github.com/storybookjs/storybook/tree/next/addons/backgrounds
+     * @see https://github.com/storybookjs/storybook/tree/next/addons/controls
+     * @see https://github.com/storybookjs/storybook/tree/next/addons/docs
+     * @see https://github.com/storybookjs/storybook/tree/next/addons/viewport
+     * @see https://github.com/storybookjs/storybook/tree/next/addons/toolbars
+     *
+     * You can disable addons you don't want through configuration.
+     * @see https://github.com/storybookjs/storybook/tree/master/addons/essentials#disabling-addons
+     */
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: true,
+        backgrounds: true,
+        controls: true,
+        docs: true,
+        viewport: true,
+        toolbars: true,
+      },
+    },
 
     /**
      * We use Google Analytics for tracking analytics usage.
