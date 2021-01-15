414733dbb03eaa308b0d232cf3551ccee94599b8
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-01-15 17:13:23 +0100

    Add CircleBtn.stories.tsx

---
+++ src/stories/nrn/form/CircleBtn.stories.tsx
@@ -0,0 +1,36 @@
+import {
+  Meta,
+  Story,
+} from '@storybook/react/types-6-0';
+import React from 'react';
+import CircleBtn, { Props } from '../../../components/utils/CircleBtn';
+import withChildrenMock from '../../shared/hocs/withChildrenMock';
+
+type PropsWithChildrenMock = Props & {
+  text?: string;
+};
+
+export default {
+  title: 'Next Right Now/Form/CircleBtn',
+  component: CircleBtn,
+  argTypes: withChildrenMock({}),
+} as Meta;
+
+const Template: Story<PropsWithChildrenMock> = (props) => {
+  const { text } = props;
+
+  return (
+    // @ts-ignore
+    <CircleBtn
+      {...props}
+      onClick={(): void => console.info('Click')}
+    >
+      {text || 'Default text'}
+    </CircleBtn>
+  );
+};
+
+export const DynamicExample: Story<PropsWithChildrenMock> = Template.bind({});
+DynamicExample.args = {
+  text: '42',
+};
