b5adec5b4938a14e31da61986404aa4ef3c1128e
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-01-19 18:36:25 +0100

    Add documentation

--- src/modules/app/getComponentName.ts
+++ src/modules/app/getComponentName.ts
@@ -3,6 +3,14 @@ import {
   NextPageContext,
 } from 'next';
 
+/**
+ * Resolves the name of a Next.js "pageProps.Component".
+ * A "Component" is a "Page".
+ *
+ * Extract the name from the component function source code.
+ *
+ * @param Component
+ */
 export const getComponentName = (Component: NextComponentType<NextPageContext>): string | null => {
   try {
     const componentAsString = Component.toString();
