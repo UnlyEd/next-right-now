0c3f4aa95384d74d5d43d7fda41082dfaa3a89ec
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-01-20 19:52:08 +0100

    Update doc

--- src/app/types/CommonServerSideParams.ts
+++ src/app/types/CommonServerSideParams.ts
@@ -1,13 +1,13 @@
 import { ParsedUrlQuery } from 'querystring';
 
 /**
- * Server side params provided to any page (SSG or SSR)
- *  - Static params provided to getStaticProps and getStaticPaths for static pages (when building SSG pages)
- *  - Dynamic params provided to getServerSideProps (when using SSR)
+ * Server side params provided to any page (SSG or SSR), no matter what layout is used.
+ *  - Static params provided to getStaticProps and getStaticPaths for static pages (when building SSG pages).
+ *  - Dynamic params provided to getServerSideProps (when using SSR).
  *
- * Those params come from the route (url) being used, they are affected by "redirects" and the route name (e.g: "/folder/[id].tsx"
+ * Those params come from the route (url) being used, they are affected by "redirects" and the route name (e.g: "/folder/[id].tsx".
  *
- * @see next.config.js "redirects" section for url params
+ * @see next.config.js "redirects" section for url params.
  */
 export type CommonServerSideParams<E extends ParsedUrlQuery = ParsedUrlQuery> = {
   albumId?: string; // Used by album-[albumId]-with-ssg-and-fallback page
