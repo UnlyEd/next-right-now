d1e11c7f16d70642d1afb244b713cacd2ce5abae
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-01-20 19:26:02 +0100

    Misc dead code

--- src/modules/core/serializeSafe/serializeSafe.ts
+++ src/modules/core/serializeSafe/serializeSafe.ts
@@ -1,6 +1,6 @@
 import { stringify as stringifySafe } from 'flatted';
-import { Flatted } from './types/Flatted';
 import { GenericObject } from '../data/types/GenericObject';
+import { Flatted } from './types/Flatted';
 
 export const serializeSafe = <T>(value: GenericObject | any[]): Flatted<T> => {
   return stringifySafe(value);
