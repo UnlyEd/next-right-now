faccc8a4fb45db86b82b77d47d1aa1aa255b4ec1
Hugo MARTIN <hugo.martin@epitech.eu>
2021-02-22 17:31:13 +0100

    who's starting npm scripts

--- scripts/populate-git-env.sh
+++ scripts/populate-git-env.sh
@@ -3,6 +3,8 @@
 
 # XXX To print those variables for testing purpose, use: `yarn script:populate-git-env:print`
 
+whoami
+
 export "GIT_COMMIT_SHA=${GIT_COMMIT_SHA:-$(yarn --silent git:getCommitSHA)}"
 export "GIT_COMMIT_REF=${GIT_COMMIT_REF:-$(yarn --silent git:getCommitRef)}"
 export "GIT_COMMIT_TAGS=${GIT_COMMIT_TAGS:-$(yarn --silent git:getReleasesAndTags)}"
