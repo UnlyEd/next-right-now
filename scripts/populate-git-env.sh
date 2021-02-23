d6e28ee428e899444492cba839fd051c1b810d85
Hugo MARTIN <hugo.martin@epitech.eu>
2021-02-23 15:52:24 +0100

    Removed whoami debug

--- scripts/populate-git-env.sh
+++ scripts/populate-git-env.sh
@@ -3,8 +3,6 @@
 
 # XXX To print those variables for testing purpose, use: `yarn script:populate-git-env:print`
 
-whoami
-
 export "GIT_COMMIT_SHA=${GIT_COMMIT_SHA:-$(yarn --silent git:getCommitSHA)}"
 export "GIT_COMMIT_REF=${GIT_COMMIT_REF:-$(yarn --silent git:getCommitRef)}"
 export "GIT_COMMIT_TAGS=${GIT_COMMIT_TAGS:-$(yarn --silent git:getReleasesAndTags)}"
