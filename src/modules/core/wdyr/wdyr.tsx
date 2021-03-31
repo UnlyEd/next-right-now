360623d7137818d683cc5a93a7b813c5ce74b8a0
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-31 14:31:07 +0200

    Remove WDYR dependency

--- src/modules/core/wdyr/wdyr.tsx
+++ src/modules/core/wdyr/wdyr.tsx
@@ -11,20 +11,20 @@
  * @see https://github.com/welldone-software/why-did-you-render
  * @see https://github.com/vercel/next.js/tree/canary/examples/with-why-did-you-render
  */
-import React from 'react';
-
-if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
-
-  // eslint-disable-next-line no-console
-  console.debug('Applying whyDidYouRender, to help you locate unnecessary re-renders during development. See https://github.com/welldone-software/why-did-you-render');
-
-  // See https://github.com/welldone-software/why-did-you-render#options
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-    trackHooks: true,
-    logOwnerReasons: true,
-    collapseGroups: true,
-  });
-}
+// import React from 'react';
+//
+// if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+//   // eslint-disable-next-line @typescript-eslint/no-var-requires
+//   const whyDidYouRender = require('@welldone-software/why-did-you-render');
+//
+//   // eslint-disable-next-line no-console
+//   console.debug('Applying whyDidYouRender, to help you locate unnecessary re-renders during development. See https://github.com/welldone-software/why-did-you-render');
+//
+//   // See https://github.com/welldone-software/why-did-you-render#options
+//   whyDidYouRender(React, {
+//     trackAllPureComponents: true,
+//     trackHooks: true,
+//     logOwnerReasons: true,
+//     collapseGroups: true,
+//   });
+// }
