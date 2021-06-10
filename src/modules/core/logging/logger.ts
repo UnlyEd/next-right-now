3f0ab40877d1b03766fb5efa56e178f6630f22c0
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-06-10 11:18:59 +0200

    Remove logging filters

--- src/modules/core/logging/logger.ts
+++ src/modules/core/logging/logger.ts
@@ -22,9 +22,9 @@ export const createLogger = ({ fileLabel }: { fileLabel: string }): SimpleLogger
       console.debug('IS_SERVER_INITIAL_BUILD', process.env.IS_SERVER_INITIAL_BUILD);
       // When bundling with Webpack, only print errors/warnings to avoid printing too much noise on Vercel
       // IS_SERVER_INITIAL_BUILD is always "1" during development, and is being ignored
-      if (process.env.NEXT_PUBLIC_APP_STAGE !== 'development' && process.env.IS_SERVER_INITIAL_BUILD === '1') {
-        return mode === 'error' || mode === 'warn' || mode === 'debug';
-      }
+      // if (process.env.NEXT_PUBLIC_APP_STAGE !== 'development' && process.env.IS_SERVER_INITIAL_BUILD === '1') {
+      //   return mode === 'error' || mode === 'warn' || mode === 'debug';
+      // }
 
       // Otherwise, only hide browser errors in production
       return !(process.env.NEXT_PUBLIC_APP_STAGE === 'production' && isBrowser());
