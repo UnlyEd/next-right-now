815ff955e7bcb232aaf0b6d0349465a1e83200a1
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-06-22 20:52:40 +0200

    Fix ingesting (eventsIngested was 0 because device_id and user_id weren't defined)

--- src/modules/core/amplitude/amplitudeServerClient.ts
+++ src/modules/core/amplitude/amplitudeServerClient.ts
@@ -3,12 +3,20 @@ import { GenericObject } from '@/modules/core/data/types/GenericObject';
 import { createLogger } from '@/modules/core/logging/logger';
 import { init } from '@amplitude/node';
 import { LogLevel } from '@amplitude/types/dist/src/logger';
+import { Response } from '@amplitude/types/dist/src/response';
 import * as Sentry from '@sentry/node';
 import startsWith from 'lodash.startswith';
 
 const fileLabel = 'modules/core/amplitude/amplitudeServerClient';
 const logger = createLogger({
   fileLabel,
+});
+
+const DEFAULT_DEVICE_ID = 'server';
+
+const amplitudeServerClient = init(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY, {
+  debug: process.env.NEXT_PUBLIC_APP_STAGE !== 'production',
+  logLevel: process.env.NEXT_PUBLIC_APP_STAGE !== 'production' ? LogLevel.Verbose : LogLevel.Error,
 });
 
 /**
@@ -28,15 +36,11 @@ export const logEvent = async (eventName: AMPLITUDE_EVENTS, userId: string, prop
   try {
     logger.info(`Logging Amplitude event "${eventName}"${userId ? ` for user "${userId}"` : ''} with properties:`, props);
 
-    const amplitudeServerClient = init(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY, {
-      debug: process.env.NEXT_PUBLIC_APP_STAGE !== 'production',
-      logLevel: process.env.NEXT_PUBLIC_APP_STAGE !== 'production' ? LogLevel.Verbose : LogLevel.Error,
-    });
-    logger.log(amplitudeServerClient);
-
     amplitudeServerClient.logEvent({
       event_type: eventName,
-      user_id: userId || '', // User id must be set (even if empty) for the event to be sent correctly
+      // Either user_id or device_id must be set, they can't both be empty or "unknown" or "eventsIngested" will be set to 0 in the response
+      user_id: userId || undefined,
+      device_id: userId ? undefined : DEFAULT_DEVICE_ID,
       event_properties: {
         'customer.ref': process.env.NEXT_PUBLIC_CUSTOMER_REF,
         ...props,
@@ -48,19 +52,27 @@ export const logEvent = async (eventName: AMPLITUDE_EVENTS, userId: string, prop
     // Send any events that are currently queued for sending.
     // Will automatically happen on the next event loop.
     // XXX It's necessary to await, or it might not work properly - See https://vercel.com/docs/platform/limits#streaming-responses
-    const response = await amplitudeServerClient.flush();
-    console.log('response', response);
+    const response: Response = await amplitudeServerClient.flush();
 
     // Monitor non 2XX response codes to allow for advanced debugging of edge cases
     if (!startsWith(response?.statusCode?.toString(10), '2')) {
       const message = `Amplitude event didn't return 200 response.`;
       logger.error(message, response);
       Sentry.withScope((scope) => {
-        scope.setExtra('response', response);
+        scope.setContext('response', response);
         Sentry.captureException(message);
       });
+    } else {
+      // @ts-ignore
+      if (response?.body?.eventsIngested === 0) {
+        const message = `Amplitude event wasn't ingested (it was sent, but not stored in Amplitude).`;
+        logger.error(message, response);
+        Sentry.withScope((scope) => {
+          scope.setContext('response', response);
+          Sentry.captureException(message);
+        });
+      }
     }
-    // TODO trouver origine warning
   } catch (e) {
     logger.error(e);
     Sentry.captureException(e);
