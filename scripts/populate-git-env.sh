dbe23254018b8bb96897f7f9482a4b1c00bf99b5
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-06-25 11:14:34 +0200

    Test Vercel ENV (unrelated)

--- scripts/populate-git-env.sh
+++ scripts/populate-git-env.sh
@@ -6,3 +6,4 @@
 export "GIT_COMMIT_SHA=${GIT_COMMIT_SHA:-$(yarn --silent git:getCommitSHA)}"
 export "GIT_COMMIT_REF=${GIT_COMMIT_REF:-$(yarn --silent git:getCommitRef)}"
 export "GIT_COMMIT_TAGS=${GIT_COMMIT_TAGS:-$(yarn --silent git:getReleasesAndTags)}"
+export "VERCEL_GIT_COMMIT_MESSAGE=Fake commit for testing Vercel"
